0e7caeafb78643236b9e699ef3a348ee9b50d5b2
0122takato <takato.buran0122@icloud.com>
2022-03-01 13:49:28 +0900

    関数verに変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 const Header = () => {
+    const componentName = () => 'Header';
     return(
-        <div>テスト</div>
+        <div className="test-wrapper">このコンポーネントの名前は{componentName()}です。</div>
     );
 };
