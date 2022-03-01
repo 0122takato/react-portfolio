648c10f741aaae6fe65e9c3c444c29cc92e99035
0122takato <takato.buran0122@icloud.com>
2022-03-01 14:14:10 +0900

    ファイル画像変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export const Header = () => {
                         {/* カバー画像 */}
                         <div className="profile-thumb" style={{ backgroundImage: `url(${ProfileImage})` }}></div>
                         {/* 名前と肩書はみなさんのお名前や肩書を自由に入れてください */}
-                        <h1 className="title-text">あなたの名前</h1>
+                        <h1 className="title-text">新谷威人</h1>
                         <h3 className="title-text">あなたの肩書(ex.Engineer)</h3>
                         <ul className="social-icons">
                             <li className="icon-link">
