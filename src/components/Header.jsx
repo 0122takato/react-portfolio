080d41505666b33ab949d048a60fe9b02ec48b25
0122takato <takato.buran0122@icloud.com>
2022-03-01 14:03:48 +0900

    cssいじり

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 export const Header = () => {
     const componentName = () => 'Header';
     return(
-        <div className="test-wrapper">このコンポーネントの名前は{componentName()}です。</div>
+        <div className="test-wrapper">
+            <p className="text">
+                このコンポーネントの名前は{componentName()}です。
+            </p>
+        </div>
     );
 };
